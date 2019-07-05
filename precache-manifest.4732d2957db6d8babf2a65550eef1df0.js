abfc9492395e85c0139b699011ecccc3599ed87e
jackson <jackson.ward21@gmail.comgit config --global user.name jacksonward>
2019-07-05 14:03:06 -0500

    Updates

---
+++ precache-manifest.4732d2957db6d8babf2a65550eef1df0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b37cdc1e7b7cf3c8573d09302c53ab04",
+    "url": "/college-countdown/index.html"
+  },
+  {
+    "revision": "53021c011627a5dd0fb6",
+    "url": "/college-countdown/static/css/main.f3fd763c.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/college-countdown/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "53021c011627a5dd0fb6",
+    "url": "/college-countdown/static/js/main.a57dc203.chunk.js"
+  },
+  {
+    "revision": "fa7e66623aa7dc2b19ba",
+    "url": "/college-countdown/static/js/runtime~main.84a3a931.js"
+  }
+]);
